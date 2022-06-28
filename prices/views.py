6ef6d139b2c7e7fba50adc31035995dde2899422
Christiano <christiano_godinho@id.uff.br>
2022-06-28 18:43:55 -0300

    URLs

--- prices/views.py
+++ prices/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
+
+def home(request):
+    return HttpResponse("Hello, world. You're at the polls index.")
 # Create your views here.
