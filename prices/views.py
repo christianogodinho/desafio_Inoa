e680560d4cf346393f4999fb31ce9870048c5104
Christiano <christiano_godinho@id.uff.br>
2022-07-04 13:58:14 -0300

    Tables

--- prices/views.py
+++ prices/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from .models import Price
-from django.http import HttpResponse
-from datetime import datetime
+from .utils import get_plot
 
 
 def home(request):
 
     price_list = Price.objects.all()
+    x = [x.codigo for x in price_list]
+    y = [y.qtd_Teorica for y in price_list]
+    chart = get_plot(x, y)
 
     return render(request, 'prices/home.html',
-                  {'price_list': price_list})
+                  {'chart': chart})
